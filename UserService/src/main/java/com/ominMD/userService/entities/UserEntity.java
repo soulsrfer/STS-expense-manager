053d8f2f0f26402ddda92131c703beae270268a8
chirag asari <71378300+soulsrfer@users.noreply.github.com>
2023-05-29 14:45:07 +0530

    userEntity added

---
+++ UserService/src/main/java/com/ominMD/userService/entities/UserEntity.java
@@ -0,0 +1,15 @@
+package com.ominMD.userService.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class UserEntity {
+
+}
