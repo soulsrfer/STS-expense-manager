790b97d18bd9ecee58dd891294389426a2f3aa91
chirag asari <71378300+soulsrfer@users.noreply.github.com>
2023-06-01 11:35:54 +0530

    update service config

--- ConfigService/src/main/java/com/ominMD/configService/ConfigServiceApplication.java
+++ ConfigService/src/main/java/com/ominMD/configService/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.ominMD.configService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
